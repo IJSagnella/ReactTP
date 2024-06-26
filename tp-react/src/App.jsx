a838121a3cc3fa49de3b377643f8d6ede6bedffb
Elizabeth Sanchez Teran <115377503+ElizabethST18@users.noreply.github.com>
2024-06-26 04:03:05 -0300

    realice cambios en el Inicio

--- tp-react/src/App.jsx
+++ tp-react/src/App.jsx
@@ -5,22 +5,29 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Inicio } from './pages/Inicio'
 import { Login } from './pages/Login'
 import { Contacto } from './pages/Contacto'
-import { Algo } from './pages/Algo'
+import { Consulta } from './pages/Consulta'
 //Menu
-import { Menu } from './components/Menu'
+import { Nav } from './components/Nav'
+import { Footer } from './components/Footer'
+import { Layout } from './components/Layout'
+
+import { AuthProvider } from './context/AuthContext'
 
 export const App = () => {
 
   return (
+    <AuthProvider>
     <BrowserRouter>
-    <Menu/>
-        <Routes>
-          <Route path= '/' element={<Inicio/>} />
-          <Route path= '/login' element={<Login/>} />
-          <Route path= '/contacto' element={<Contacto/>} />
-          <Route path= '/algo' element={<Algo/>} />
-        </Routes>
+    <Nav/>
+      <Routes>
+        <Route path= '/' element={<Inicio/>} />
+        <Route path= '/contacto' element={<Contacto/>} />
+        <Route path= '/consulta' element={<Consulta/>} />
+        <Route path= '/login' element={<Login/>} />
+      </Routes>
+    <Footer/>
     </BrowserRouter>
+    </AuthProvider>
   )
 }
 export default App
