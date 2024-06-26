c349aac1a9b2b2ad61e79b30f0bd2479c44d10f3
Elizabeth Sanchez Teran <115377503+ElizabethST18@users.noreply.github.com>
2024-06-26 04:12:01 -0300

    creo AuthContext

---
+++ tp-react/src/context/AuthContext.jsx
@@ -0,0 +1,30 @@
+import { createContext, useContext, useState } from "react";
+
+const AuthContext = createContext();
+
+export const AuthProvider = ( {children} ) => {
+
+    const [logueado, setLogueado] = useState(false);
+    const [token, setToken] = useState(null);
+
+    const login = (accessToken) => {
+        setLogueado(true);
+        setToken(accessToken);
+    }
+
+    const logout = () => {
+        setLogueado(false);
+        setToken(null);
+    }
+
+    return (
+        <AuthContext.Provider value={ {logueado, login, logout, token} }>
+            { children }
+        </AuthContext.Provider>
+    )
+
+}
+
+export const useAuth = () => {
+    return useContext(AuthContext);
+}
