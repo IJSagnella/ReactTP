1c89dc42e97ea29dfcef217247d3cd43d617487e
Elizabeth Sanchez Teran <115377503+ElizabethST18@users.noreply.github.com>
2024-07-04 23:47:54 -0300

    cambios en los import y brouserRouter

--- tp-react/src/App.jsx
+++ tp-react/src/App.jsx
@@ -1,33 +1,40 @@
 import './App.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 //importo las Paginas
 import { Inicio } from './pages/Inicio'
 import { Login } from './pages/Login'
 import { Contacto } from './pages/Contacto'
 import { Consulta } from './pages/Consulta'
+import {Empleado} from './pages/Empleado'
+import {Admin} from './pages/Admin'
+
 //Menu
+
 import { Nav } from './components/Nav'
 import { Footer } from './components/Footer'
 import { Layout } from './components/Layout'
-
+//Contextos
 import { AuthProvider } from './context/AuthContext'
 
 export const App = () => {
 
   return (
+
+    <Router>
     <AuthProvider>
-    <BrowserRouter>
     <Nav/>
       <Routes>
         <Route path= '/' element={<Inicio/>} />
         <Route path= '/contacto' element={<Contacto/>} />
         <Route path= '/consulta' element={<Consulta/>} />
         <Route path= '/login' element={<Login/>} />
+        <Route path= '/empleado' element={<Empleado/>} />
+        <Route path= '/admin' element={<Admin/>} />
       </Routes>
     <Footer/>
-    </BrowserRouter>
     </AuthProvider>
+    </Router>
   )
 }
 export default App
