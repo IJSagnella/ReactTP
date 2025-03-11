c9071042274971a875af55a964f96c7a778038bc
Ignacio Javier Sagnella <ignacio.sagnella@davinci.edu.ar>
2025-03-11 12:59:46 -0300

    configuracion de db

--- tp-backend/db.js
+++ tp-backend/db.js
@@ -3,10 +3,10 @@ const mysql = require('mysql2/promise');
 
 // Configuración de la conexión a la base de datos
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'teleservice_db',
+  host: process.env.DATA_BASE_HOST,
+  user: process.env.DATA_BASE_USER,
+  password: process.env.DATA_BASE_PASSWORD,
+  database: process.env.DATA_BASE_NAME,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
